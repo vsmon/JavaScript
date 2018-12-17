7f46ee74077aae0e67737bb7d9c6b7ab9833daa9
Rodrigo F Martins <rodrigo.fonsecamartins@gmail.com>
2018-12-17 16:37:34 -0200

    Adicionado exemplo3

---
+++ Exemplo3/script.js
@@ -0,0 +1,7 @@
+function addBola(){
+    bola = document.createElement("div");
+    bola.id = "bola";
+    bola.style.left = "100px";
+    bola.style.top = "100px";
+    document.body.appendChild(bola);
+}
