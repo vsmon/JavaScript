a308dcdfc3441a1998114b4cecfae312d22f57fe
Rodrigo F Martins <rodrigo.fonsecamartins@gmail.com>
2018-12-18 10:02:28 -0200

    Adicionado remover bola

--- Exemplo3/script.js
+++ Exemplo3/script.js
@@ -1,16 +1,19 @@
 function addBola(){
     
-    left = Math.floor(Math.random() * 500);
-    top  = Math.floor(Math.random() * 500);
-    console.log(left);
+    p1Left = Math.floor(Math.random() * 1300);
+    p2Top  = Math.floor(Math.random() * 500);
+    console.log(p2Top);
     bola = document.createElement("div");
     bola.id = "bola";
-    bola.style.left = `${left}px`;
-    bola.style.top = `${top}px`;
+    bola.style.left = `${p1Left}px`;
+    bola.style.top = `${p2Top}px`;
     bola.setAttribute("onClick", "removerBola(this)");
     document.body.appendChild(bola);
     
 }
 function removerBola(elemento){
     document.body.removeChild(elemento);
+}
+function iniciar(){
+    setInterval("addBola()", 1000);
 }
