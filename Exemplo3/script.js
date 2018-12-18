50bda230656226ebccf375419ac42d8f494e3bb3
Rodrigo <rodrigo.fonsecamartins@gmail.com>
2018-12-17 23:34:19 -0200

    Adicionado a funcao removerBola

--- Exemplo3/script.js
+++ Exemplo3/script.js
@@ -1,7 +1,16 @@
 function addBola(){
+    
+    left = Math.floor(Math.random() * 500);
+    top  = Math.floor(Math.random() * 500);
+    console.log(left);
     bola = document.createElement("div");
     bola.id = "bola";
-    bola.style.left = "100px";
-    bola.style.top = "100px";
+    bola.style.left = left+"px";
+    bola.style.top = top+"px";
+    bola.setAttribute("onClick", "removerBola(this)");
     document.body.appendChild(bola);
+    
+}
+function removerBola(elemento){
+    document.body.removeChild(elemento);
 }
